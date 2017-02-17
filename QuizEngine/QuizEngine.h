16ef8ef0bcc5125b44571078631702d0671653c5
Caio Zullo <caiozullo@gmail.com>
2017-02-17 12:08:17 +0000

    Initial Commit

---
+++ QuizEngine/QuizEngine.h
@@ -0,0 +1,19 @@
+//
+//  QuizEngine.h
+//  QuizEngine
+//
+//  Created by Caio Zullo on 17/02/2017.
+//  Copyright © 2017 Essential Developer. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for QuizEngine.
+FOUNDATION_EXPORT double QuizEngineVersionNumber;
+
+//! Project version string for QuizEngine.
+FOUNDATION_EXPORT const unsigned char QuizEngineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QuizEngine/PublicHeader.h>
+
+
