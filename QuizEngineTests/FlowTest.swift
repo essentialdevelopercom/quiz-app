a0455beedfc0e29a09bf00da7be6144c12d50f50
Caio Zullo <caiozullo@gmail.com>
2017-02-27 12:47:44 +0000

    Add missing tests

--- QuizEngineTests/FlowTest.swift
+++ QuizEngineTests/FlowTest.swift
@@ -68,6 +68,21 @@ class FlowTest: XCTestCase {
         XCTAssertEqual(router.routedResult!, [:])
     }
     
+    func test_start_withOneQuestions_doesNotRouteToResult() {
+        makeSUT(questions: ["Q1"]).start()
+        
+        XCTAssertNil(router.routedResult)
+    }
+    
+    func test_startAndAnswerFirstQuestion_withTwoQuestions_doesNotRouteToResult() {
+        let sut = makeSUT(questions: ["Q1", "Q2"])
+        sut.start()
+        
+        router.answerCallback("A1")
+        
+        XCTAssertNil(router.routedResult)
+    }
+
     func test_startAndAnswerFirstAndSecondQuestion_withTwoQuestions_routesToResult() {
         let sut = makeSUT(questions: ["Q1", "Q2"])
         sut.start()
