497f4b34dc556960ddcbd0fd551024e564bd9aee
Caio Zullo <caiozullo@gmail.com>
2017-03-02 14:48:54 +0000

    End of Tomato 13 - Question enum type

---
+++ App/QuizApp/Question.swift
@@ -0,0 +1,30 @@
+//
+//  Copyright © 2017 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+enum Question<T: Hashable> : Hashable {
+    case singleAnswer(T)
+    case multipleAnswer(T)
+    
+    var hashValue: Int {
+        switch self {
+        case .singleAnswer(let value):
+            return value.hashValue
+        case .multipleAnswer(let value):
+            return value.hashValue
+        }
+    }
+    
+    static func ==(lhs: Question<T>, rhs: Question<T>) -> Bool {
+        switch (lhs, rhs) {
+        case (.singleAnswer(let a), .singleAnswer(let b)):
+            return a == b
+        case (.multipleAnswer(let a), .multipleAnswer(let b)):
+            return a == b
+        default:
+            return false
+        }
+    }
+}
---
+++ App/QuizAppTests/QuestionTest.swift
@@ -0,0 +1,39 @@
+//
+//  Copyright © 2017 Essential Developer. All rights reserved.
+//
+
+import Foundation
+import XCTest
+@testable import QuizApp
+
+class QuestionTest: XCTestCase {
+
+    func test_hashValue_singleAnswer_returnsTypeHash() {
+        let type = "a string"
+        
+        let sut = Question.singleAnswer(type)
+        
+        XCTAssertEqual(sut.hashValue, type.hashValue)
+    }
+    
+    func test_hashValue_multipleAnswer_returnsTypeHash() {
+        let type = "a string"
+        
+        let sut = Question.multipleAnswer(type)
+        
+        XCTAssertEqual(sut.hashValue, type.hashValue)
+    }
+    
+    func test_equal_isEqual() {
+        XCTAssertEqual(Question.singleAnswer("a string"), Question.singleAnswer("a string"))
+        XCTAssertEqual(Question.multipleAnswer("a string"), Question.multipleAnswer("a string"))
+    }
+
+    func test_notEqual_isNotEqual() {
+        XCTAssertNotEqual(Question.singleAnswer("a string"), Question.singleAnswer("another string"))
+        XCTAssertNotEqual(Question.multipleAnswer("a string"), Question.multipleAnswer("another string"))
+        XCTAssertNotEqual(Question.singleAnswer("a string"), Question.multipleAnswer("another string"))
+        XCTAssertNotEqual(Question.singleAnswer("a string"), Question.multipleAnswer("a string"))
+    }
+
+}
