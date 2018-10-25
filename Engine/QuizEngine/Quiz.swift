b76b7c57a9571735f54d0b2b6a08dc74d18928ab
Caio Zullo <caiozullo@gmail.com>
2018-10-25 18:17:38 +0100

    Fix formatting

--- Engine/QuizEngine/Quiz.swift
+++ Engine/QuizEngine/Quiz.swift
@@ -11,8 +11,16 @@ public final class Quiz {
 		self.flow = flow
 	}
 	
-	public static func start<Delegate: QuizDelegate>(questions: [Delegate.Question], delegate: Delegate, correctAnswers: [Delegate.Question: Delegate.Answer]) -> Quiz where Delegate.Answer: Equatable {
-		let flow = Flow(questions: questions, delegate: delegate, scoring: { scoring($0, correctAnswers: correctAnswers) })
+	public static func start<Delegate: QuizDelegate>(
+		questions: [Delegate.Question],
+		delegate: Delegate,
+		correctAnswers: [Delegate.Question: Delegate.Answer]
+	) -> Quiz where Delegate.Answer: Equatable {
+		let flow = Flow(
+			questions: questions,
+			delegate: delegate,
+			scoring: { scoring($0, correctAnswers: correctAnswers) }
+		)
 		flow.start()
 		return Quiz(flow: flow)
 	}
