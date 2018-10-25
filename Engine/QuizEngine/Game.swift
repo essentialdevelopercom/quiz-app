93edc27a51a82a74a03eb9407024e03fd678663d
Caio Zullo <caiozullo@gmail.com>
2018-10-25 16:01:34 +0100

    Deprecated the startGame method

--- Engine/QuizEngine/Game.swift
+++ Engine/QuizEngine/Game.swift
@@ -12,6 +12,7 @@ public class Game <Question, Answer, R: Router> where R.Question == Question, R.
     }
 }
 
+@available(*, deprecated)
 public func startGame<Question, Answer: Equatable, R: Router>(questions: [Question], router: R, correctAnswers: [Question: Answer]) -> Game<Question, Answer, R> {
     let flow = Flow(questions: questions, router: router, scoring: { scoring($0, correctAnswers: correctAnswers) })
     flow.start()
