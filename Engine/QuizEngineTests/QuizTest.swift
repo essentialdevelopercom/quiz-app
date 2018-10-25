4aee1a7d9eede560768834c0a5fb7ed7c26c1784
Caio Zullo <caiozullo@gmail.com>
2018-10-25 17:49:20 +0100

    Duplicated the deprecated test. It will be the scaffolding for the new APIs

---
+++ Engine/QuizEngineTests/QuizTest.swift
@@ -0,0 +1,55 @@
+//
+//  Copyright © 2018 Essential Developer. All rights reserved.
+//
+
+import Foundation
+import XCTest
+import QuizEngine
+
+class QuizTest: XCTestCase {
+	
+	private let router = RouterSpy()
+	private var game: Game<String, String, RouterSpy>!
+	
+	override func setUp() {
+		super.setUp()
+		
+		game = startGame(questions: ["Q1", "Q2"], router: router, correctAnswers: ["Q1": "A1", "Q2": "A2"])
+	}
+	
+	func test_startGame_answerZeroOutOfTwoCorrectly_scoresZero() {
+		router.answerCallback("wrong")
+		router.answerCallback("wrong")
+		
+		XCTAssertEqual(router.routedResult!.score, 0)
+	}
+	
+	func test_startGame_answerOneOutOfTwoCorrectly_scoresOne() {
+		router.answerCallback("A1")
+		router.answerCallback("wrong")
+		
+		XCTAssertEqual(router.routedResult!.score, 1)
+	}
+	
+	func test_startGame_answerTwoOutOfTwoCorrectly_scoresTwo() {
+		router.answerCallback("A1")
+		router.answerCallback("A2")
+		
+		XCTAssertEqual(router.routedResult!.score, 2)
+	}
+	
+	private class RouterSpy: Router {
+		var routedResult: Result<String, String>? = nil
+		
+		var answerCallback: (String) -> Void = { _ in }
+		
+		func routeTo(question: String, answerCallback: @escaping (String) -> Void) {
+			self.answerCallback = answerCallback
+		}
+		
+		func routeTo(result: Result<String, String>) {
+			routedResult = result
+		}
+	}
+	
+}
