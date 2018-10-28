c5b72f58622072f36da51fd51c14e6ca83a6a158
Caio Zullo <caiozullo@gmail.com>
2018-10-28 16:54:51 +0000

    Updated the deprecated adapter to use the new didCompleteQuiz API

--- Engine/QuizEngine/DeprecatedGame.swift
+++ Engine/QuizEngine/DeprecatedGame.swift
@@ -24,24 +24,35 @@ public class Game <Question, Answer, R: Router> {
 
 @available(*, deprecated)
 public func startGame<Question, Answer: Equatable, R: Router>(questions: [Question], router: R, correctAnswers: [Question: Answer]) -> Game<Question, Answer, R> where R.Question == Question, R.Answer == Answer {
-    let flow = Flow(questions: questions, delegate: QuizDelegateToRouterAdapter(router), scoring: { scoring($0, correctAnswers: correctAnswers) })
+    let flow = Flow(questions: questions, delegate: QuizDelegateToRouterAdapter(router, correctAnswers), scoring: { scoring($0, correctAnswers: correctAnswers) })
     flow.start()
     return Game(flow: flow)
 }
 
 @available(*, deprecated)
-private class QuizDelegateToRouterAdapter<R: Router>: QuizDelegate {
+private class QuizDelegateToRouterAdapter<R: Router>: QuizDelegate where R.Answer: Equatable {
 	private let router: R
+	private let correctAnswers: [R.Question: R.Answer]
 	
-	init(_ router: R) {
+	init(_ router: R, _ correctAnswers: [R.Question: R.Answer]) {
 		self.router = router
+		self.correctAnswers = correctAnswers
 	}
 	
 	func answer(for question: R.Question, completion: @escaping (R.Answer) -> Void) {
 		router.routeTo(question: question, answerCallback: completion)
 	}
 	
-	func handle(result: Result<R.Question, R.Answer>) {
+	func didCompleteQuiz(withAnswers answers: [(question: R.Question, answer: R.Answer)]) {
+		let answersDictionary = answers.reduce([R.Question: R.Answer]()) { acc, tuple in
+			var acc = acc
+			acc[tuple.question] = tuple.answer
+			return acc
+		}
+		let score = scoring(answersDictionary, correctAnswers: correctAnswers)
+		let result = Result(answers: answersDictionary, score: score)
 		router.routeTo(result: result)
 	}
+	
+	func handle(result: Result<R.Question, R.Answer>) {}
 }
--- Engine/QuizEngineTests/QuizTest.swift
+++ Engine/QuizEngineTests/QuizTest.swift
@@ -18,22 +18,22 @@ class QuizTest: XCTestCase {
 	}
 	
 	func test_startQuiz_answerZeroOutOfTwoCorrectly_scoresZero() {
-		delegate.answerCompletion("wrong")
-		delegate.answerCompletion("wrong")
+		delegate.answerCallback("wrong")
+		delegate.answerCallback("wrong")
 		
 		XCTAssertEqual(delegate.handledResult!.score, 0)
 	}
 	
 	func test_startQuiz_answerOneOutOfTwoCorrectly_scoresOne() {
-		delegate.answerCompletion("A1")
-		delegate.answerCompletion("wrong")
+		delegate.answerCallback("A1")
+		delegate.answerCallback("wrong")
 		
 		XCTAssertEqual(delegate.handledResult!.score, 1)
 	}
 	
 	func test_startQuiz_answerTwoOutOfTwoCorrectly_scoresTwo() {
-		delegate.answerCompletion("A1")
-		delegate.answerCompletion("A2")
+		delegate.answerCallback("A1")
+		delegate.answerCallback("A2")
 		
 		XCTAssertEqual(delegate.handledResult!.score, 2)
 	}
@@ -41,10 +41,10 @@ class QuizTest: XCTestCase {
 	private class DelegateSpy: QuizDelegate {
 		var handledResult: Result<String, String>? = nil
 		
-		var answerCompletion: (String) -> Void = { _ in }
+		var answerCallback: (String) -> Void = { _ in }
 		
 		func answer(for: String, completion: @escaping (String) -> Void) {
-			self.answerCompletion = completion
+			self.answerCallback = completion
 		}
 		
 		func handle(result: Result<String, String>) {
