2a92619aa924962e730c59e5f34d57685fe0f6fb
Caio Zullo <caiozullo@gmail.com>
2018-10-29 11:09:25 +0000

    Scores one for correct answer

--- Engine/QuizEngineTests/ScoreTest.swift
+++ Engine/QuizEngineTests/ScoreTest.swift
@@ -15,9 +15,14 @@ class ScoreTest: XCTestCase {
 		XCTAssertEqual(BasicScore.score(for: ["wrong"], comparingTo: ["correct"]), 0)
 	}
 	
+	func test_oneCorrectAnswer_scoresOne() {
+		XCTAssertEqual(BasicScore.score(for: ["correct"], comparingTo: ["correct"]), 1)
+	}
+	
 	private class BasicScore {
-		static func score(for: [Any], comparingTo: [Any]) -> Int {
-			return 0
+		static func score(for answers: [String], comparingTo correctAnswers: [String]) -> Int {
+			if answers.isEmpty { return 0 }
+			return answers == correctAnswers ? 1 : 0
 		}
 	}
 	
