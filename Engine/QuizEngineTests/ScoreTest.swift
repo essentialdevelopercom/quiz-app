59cd025f6e4fd68ade2ba8a4b32f6ffa16edc1cb
Caio Zullo <caiozullo@gmail.com>
2018-10-29 11:13:36 +0000

    Can score a correct answer along with a wrong answer

--- Engine/QuizEngineTests/ScoreTest.swift
+++ Engine/QuizEngineTests/ScoreTest.swift
@@ -19,10 +19,21 @@ class ScoreTest: XCTestCase {
 		XCTAssertEqual(BasicScore.score(for: ["correct"], comparingTo: ["correct"]), 1)
 	}
 	
+	func test_oneCorrectAnswerOneWrong_scoresOne() {
+		let score = BasicScore.score(
+			for: ["correct 1", "wrong"],
+			comparingTo: ["correct 1", "correct 2"])
+		
+		XCTAssertEqual(score, 1)
+	}
+
 	private class BasicScore {
 		static func score(for answers: [String], comparingTo correctAnswers: [String]) -> Int {
-			if answers.isEmpty { return 0 }
-			return answers == correctAnswers ? 1 : 0
+			var score = 0
+			for (index, answer) in answers.enumerated() {
+				score += (answer == correctAnswers[index]) ? 1 : 0
+			}
+			return score
 		}
 	}
 	
