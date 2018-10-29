5905072ffd93a0c44438b88bb2a5af8123e16acf
Caio Zullo <caiozullo@gmail.com>
2018-10-29 09:52:57 +0000

    Quiz now allows clients to answer the same question more than once

--- Engine/QuizEngine/Flow.swift
+++ Engine/QuizEngine/Flow.swift
@@ -40,7 +40,7 @@ class Flow <Delegate: QuizDelegate> {
     
     private func answer(for question: Question, at index: Int) -> (Answer) -> Void {
         return { [weak self] answer in
-			self?.newAnswers.append((question, answer))
+			self?.newAnswers.replaceOrInsert((question, answer), at: index)
             self?.answers[question] = answer
             self?.delegateQuestionHandling(after: index)
         }
@@ -49,4 +49,13 @@ class Flow <Delegate: QuizDelegate> {
     private func result() -> Result<Question, Answer> {
         return Result(answers: answers, score: scoring(answers))
     }
+}
+
+private extension Array {
+	mutating func replaceOrInsert(_ element: Element, at index: Index) {
+		if index < count {
+			remove(at: index)
+		}
+		insert(element, at: index)
+	}
 }
--- Engine/QuizEngineTests/QuizTest.swift
+++ Engine/QuizEngineTests/QuizTest.swift
@@ -22,6 +22,22 @@ class QuizTest: XCTestCase {
 		assertEqual(delegate.completedQuizzes[0], [("Q1", "A1"), ("Q2", "A2")])
 	}
 	
+	func test_startQuiz_answersAllQuestionsTwice_completesWithNewAnswers() {
+		let delegate = DelegateSpy()
+
+		quiz = Quiz.start(questions: ["Q1", "Q2"], delegate: delegate)
+
+		delegate.answerCompletions[0]("A1")
+		delegate.answerCompletions[1]("A2")
+
+		delegate.answerCompletions[0]("A1-1")
+		delegate.answerCompletions[1]("A2-2")
+
+		XCTAssertEqual(delegate.completedQuizzes.count, 2)
+		assertEqual(delegate.completedQuizzes[0], [("Q1", "A1"), ("Q2", "A2")])
+		assertEqual(delegate.completedQuizzes[1], [("Q1", "A1-1"), ("Q2", "A2-2")])
+	}
+
 	private func assertEqual(_ a1: [(String, String)], _ a2: [(String, String)], file: StaticString = #file, line: UInt = #line) {
 		XCTAssertTrue(a1.elementsEqual(a2, by: ==), "\(a1) is not equal to \(a2)", file: file, line: line)
 	}
