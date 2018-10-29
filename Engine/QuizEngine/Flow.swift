ba1e6a243ed96cb11f87cb31606dbc249c843272
Caio Zullo <caiozullo@gmail.com>
2018-10-29 10:14:42 +0000

    Improved a property name

--- Engine/QuizEngine/Flow.swift
+++ Engine/QuizEngine/Flow.swift
@@ -10,7 +10,7 @@ class Flow <Delegate: QuizDelegate> {
     
     private let delegate: Delegate
     private let questions: [Question]
-	private var newAnswers: [(Question, Answer)] = []
+	private var answers: [(Question, Answer)] = []
 	
 	init(questions: [Question], delegate: Delegate) {
         self.questions = questions
@@ -26,7 +26,7 @@ class Flow <Delegate: QuizDelegate> {
             let question = questions[index]
             delegate.answer(for: question, completion: answer(for: question, at: index))
         } else {
-			delegate.didCompleteQuiz(withAnswers: newAnswers)
+			delegate.didCompleteQuiz(withAnswers: answers)
         }
     }
     
@@ -36,7 +36,7 @@ class Flow <Delegate: QuizDelegate> {
     
     private func answer(for question: Question, at index: Int) -> (Answer) -> Void {
         return { [weak self] answer in
-			self?.newAnswers.replaceOrInsert((question, answer), at: index)
+			self?.answers.replaceOrInsert((question, answer), at: index)
             self?.delegateQuestionHandling(after: index)
         }
     }
