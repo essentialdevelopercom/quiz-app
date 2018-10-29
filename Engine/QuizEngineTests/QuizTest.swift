a161d5387f188927e027bbb3fd64079da62d395d
Caio Zullo <caiozullo@gmail.com>
2018-10-29 09:46:40 +0000

    Capture answer completions in an ordered form

--- Engine/QuizEngineTests/QuizTest.swift
+++ Engine/QuizEngineTests/QuizTest.swift
@@ -15,8 +15,8 @@ class QuizTest: XCTestCase {
 
 		quiz = Quiz.start(questions: ["Q1", "Q2"], delegate: delegate)
 
-		delegate.answerCompletion("A1")
-		delegate.answerCompletion("A2")
+		delegate.answerCompletions[0]("A1")
+		delegate.answerCompletions[1]("A2")
 		
 		XCTAssertEqual(delegate.completedQuizzes.count, 1)
 		assertEqual(delegate.completedQuizzes[0], [("Q1", "A1"), ("Q2", "A2")])
@@ -28,10 +28,10 @@ class QuizTest: XCTestCase {
 
 	private class DelegateSpy: QuizDelegate {
 		var completedQuizzes: [[(String, String)]] = []
-		var answerCompletion: (String) -> Void = { _ in }
+		var answerCompletions: [(String) -> Void] = []
 		
 		func answer(for question: String, completion: @escaping (String) -> Void) {
-			self.answerCompletion = completion
+			self.answerCompletions.append(completion)
 		}
 		
 		func didCompleteQuiz(withAnswers answers: [(question: String, answer: String)]) {
