07eabefbd9cfb8c85a701359832ed8d24263a0c9
Caio Zullo <caiozullo@gmail.com>
2018-10-29 10:00:30 +0000

    Guarantee the flow allows clients to answer the same question more than once

--- Engine/QuizEngineTests/FlowTest.swift
+++ Engine/QuizEngineTests/FlowTest.swift
@@ -92,7 +92,22 @@ class FlowTest: XCTestCase {
 		XCTAssertEqual(delegate.completedQuizzes.count, 1)
         assertEqual(delegate.completedQuizzes[0], [("Q1", "A1"), ("Q2", "A2")])
     }
+	
+	func test_startAndAnswerFirstAndSecondQuestionTwice_withTwoQuestions_completesQuizTwice() {
+		let sut = makeSUT(questions: ["Q1", "Q2"])
+		sut.start()
 
+		delegate.answerCompletions[0]("A1")
+		delegate.answerCompletions[1]("A2")
+
+		delegate.answerCompletions[0]("A1-1")
+		delegate.answerCompletions[1]("A2-2")
+		
+		XCTAssertEqual(delegate.completedQuizzes.count, 2)
+		assertEqual(delegate.completedQuizzes[0], [("Q1", "A1"), ("Q2", "A2")])
+		assertEqual(delegate.completedQuizzes[1], [("Q1", "A1-1"), ("Q2", "A2-2")])
+	}
+	
     // MARK: Helpers
 	
 	private let delegate = DelegateSpy()
