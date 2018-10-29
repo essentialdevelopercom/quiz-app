99caefd1a93f80c6252480ad6e1126dd29ac3fbf
Caio Zullo <caiozullo@gmail.com>
2018-10-29 10:18:43 +0000

    Remove a redundant type constraint

--- Engine/QuizEngine/QuizDelegate.swift
+++ Engine/QuizEngine/QuizDelegate.swift
@@ -5,7 +5,7 @@
 import Foundation
 
 public protocol QuizDelegate {
-	associatedtype Question: Hashable
+	associatedtype Question
 	associatedtype Answer
 	
 	func answer(for question: Question, completion: @escaping (Answer) -> Void)
