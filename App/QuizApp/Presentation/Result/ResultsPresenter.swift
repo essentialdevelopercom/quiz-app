45b32a2d529aadae7625c85e285e0c2b5adc9fc6
Caio Zullo <caiozullo@gmail.com>
2018-10-30 11:19:11 +0000

    Create new ResultsPresenter APIs by maintaining backwards compatibility.

--- App/QuizApp/Presentation/Result/ResultsPresenter.swift
+++ App/QuizApp/Presentation/Result/ResultsPresenter.swift
@@ -5,30 +5,42 @@
 import Foundation
 import QuizEngine
 
-struct ResultsPresenter {
-    let result: Result<Question<String>, [String]>
-    let questions: [Question<String>]
-    let correctAnswers: Dictionary<Question<String>, [String]>
-    
+final class ResultsPresenter {
+	typealias Answers = [(question: Question<String>, answers: [String])]
+	typealias Scorer = ([[String]], [[String]]) -> Int
+	
+	private let userAnswers: Answers
+	private let correctAnswers: Answers
+	private let scorer: Scorer
+	
+	init(result: Result<Question<String>, [String]>, questions: [Question<String>], correctAnswers: Dictionary<Question<String>, [String]>) {
+		self.userAnswers = questions.map { question in
+			(question, result.answers[question]!)
+		}
+		self.correctAnswers = questions.map { question in
+			(question, correctAnswers[question]!)
+		}
+		self.scorer = { _, _ in result.score }
+	}
+	
     var title: String {
         return "Result"
     }
     
     var summary: String {
-        return "You got \(result.score)/\(result.answers.count) correct"
+        return "You got \(score)/\(userAnswers.count) correct"
     }
-    
+	
     var presentableAnswers: [PresentableAnswer] {
-        return questions.map { question in
-            guard let userAnswer = result.answers[question],
-                let correctAnswer = correctAnswers[question] else {
-                fatalError("Couldn't find correct answer for question: \(question)")
-            }
-            
-            return presentableAnswer(question, userAnswer, correctAnswer)
+        return zip(userAnswers, correctAnswers).map { userAnswer, correctAnswer in
+            return presentableAnswer(userAnswer.question, userAnswer.answers, correctAnswer.answers)
         }
     }
-    
+	
+	private var score: Int {
+		return scorer(userAnswers.map { $0.answers }, correctAnswers.map { $0.answers })
+	}
+
     private func presentableAnswer(_ question: Question<String>, _ userAnswer: [String], _ correctAnswer: [String]) -> PresentableAnswer {
         switch question {
         case .singleAnswer(let value), .multipleAnswer(let value):
