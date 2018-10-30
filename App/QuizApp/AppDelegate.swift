55bf3d7d58bf87e80a8bcaaef84a53162e98fc8c
Caio Zullo <caiozullo@gmail.com>
2018-10-30 15:16:10 +0000

    Updated the AppDelegate to use new factory API

--- App/QuizApp/AppDelegate.swift
+++ App/QuizApp/AppDelegate.swift
@@ -29,7 +29,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let correctAnswers = [question1: [option3], question2: [option4, option6]]
         
         let navigationController = UINavigationController()
-        let factory = iOSViewControllerFactory(questions: questions, options: [question1: options1, question2: options2], correctAnswers: correctAnswers)
+        let factory = iOSViewControllerFactory(
+			options: [question1: options1, question2: options2],
+			correctAnswers: [(question1, [option3]), (question2, [option4, option6])]
+		)
         let router = NavigationControllerRouter(navigationController, factory: factory)
         
         window = UIWindow(frame: UIScreen.main.bounds)
