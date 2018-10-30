145c9bbac5cf7c69601082ec4d093b0f77e464f6
Caio Zullo <caiozullo@gmail.com>
2018-10-30 14:01:44 +0000

    Removed the ResultsPresenter deprecated initializer

--- App/QuizApp/Presentation/Result/ResultsPresenter.swift
+++ App/QuizApp/Presentation/Result/ResultsPresenter.swift
@@ -19,16 +19,6 @@ final class ResultsPresenter {
 		self.scorer = scorer
 	}
 	
-	init(result: Result<Question<String>, [String]>, questions: [Question<String>], correctAnswers: Dictionary<Question<String>, [String]>) {
-		self.userAnswers = questions.map { question in
-			(question, result.answers[question]!)
-		}
-		self.correctAnswers = questions.map { question in
-			(question, correctAnswers[question]!)
-		}
-		self.scorer = { _, _ in result.score }
-	}
-	
     var title: String {
         return "Result"
     }
--- App/QuizAppTests/iOSViewControllerFactoryTest.swift
+++ App/QuizAppTests/iOSViewControllerFactoryTest.swift
@@ -89,3 +89,17 @@ class iOSViewControllerFactoryTest: XCTestCase {
         return (controller, presenter)
     }
 }
+
+private extension ResultsPresenter {
+	convenience init(result: Result<Question<String>, [String]>, questions: [Question<String>], correctAnswers: Dictionary<Question<String>, [String]>) {
+		self.init(
+			userAnswers: questions.map { question in
+				(question, result.answers[question]!)
+			},
+			correctAnswers: questions.map { question in
+				(question, correctAnswers[question]!)
+			},
+			scorer: { _, _ in result.score }
+		)
+	}
+}
