457c9075b69d8f2faa7c3e4dd10aa6d716187a01
Caio Zullo <caiozullo@gmail.com>
2018-10-31 14:53:31 +0000

    Make the internal Flow class final

--- Engine/QuizEngine/Flow.swift
+++ Engine/QuizEngine/Flow.swift
@@ -4,7 +4,7 @@
 
 import Foundation
 
-class Flow <Delegate: QuizDelegate> {
+final class Flow<Delegate: QuizDelegate> {
     typealias Question = Delegate.Question
     typealias Answer = Delegate.Answer
     
