07b53a24fec7fc7fcd271ea6f6889d2f5d35c8d8
Caio Zullo <caiozullo@gmail.com>
2018-10-31 11:03:47 +0000

    Removed a deprecated method

--- App/QuizAppTests/Routing/NavigationControllerRouterTest.swift
+++ App/QuizAppTests/Routing/NavigationControllerRouterTest.swift
@@ -144,10 +144,6 @@ class NavigationControllerRouterTest: XCTestCase {
 		func resultsViewController(for userAnswers: Answers) -> UIViewController {
 			return stubbedResults[userAnswers.map { $0.question }] ?? UIViewController()
 		}
-		
-        func resultsViewController(for result: Result<Question<String>, [String]>) -> UIViewController {
-            return UIViewController()
-        }
     }
 
 }
