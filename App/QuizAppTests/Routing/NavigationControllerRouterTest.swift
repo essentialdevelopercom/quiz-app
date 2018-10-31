762cd941c99d47e55c6cb948085fa61ac0380388
Caio Zullo <caiozullo@gmail.com>
2018-10-31 11:01:22 +0000

    Organized test file

--- App/QuizAppTests/Routing/NavigationControllerRouterTest.swift
+++ App/QuizAppTests/Routing/NavigationControllerRouterTest.swift
@@ -8,17 +8,7 @@ import QuizEngine
 @testable import QuizApp
 
 class NavigationControllerRouterTest: XCTestCase {
-    
-    let singleAnswerQuestion = Question.singleAnswer("Q1")
-    let multipleAnswerQuestion = Question.multipleAnswer("Q2")
-
-    let navigationController = NonAnimatedNavigationController()
-    let factory = ViewControllerFactoryStub()
-    
-    lazy var sut: NavigationControllerRouter = {
-        return NavigationControllerRouter(self.navigationController, factory: self.factory)
-    }()
-
+	
     func test_answerForQuestion_showsQuestionController() {
         let viewController = UIViewController()
         let secondViewController = UIViewController()
@@ -115,15 +105,24 @@ class NavigationControllerRouterTest: XCTestCase {
     }
 
     // MARK: Helpers
-    
-    class NonAnimatedNavigationController: UINavigationController {
+	
+	private let singleAnswerQuestion = Question.singleAnswer("Q1")
+	private let multipleAnswerQuestion = Question.multipleAnswer("Q2")
+	
+	private let navigationController = NonAnimatedNavigationController()
+	private let factory = ViewControllerFactoryStub()
+	
+	private lazy var sut: NavigationControllerRouter = {
+		return NavigationControllerRouter(self.navigationController, factory: self.factory)
+	}()
+
+    private class NonAnimatedNavigationController: UINavigationController {
         override func pushViewController(_ viewController: UIViewController, animated: Bool) {
             super.pushViewController(viewController, animated: false)
         }
     }
     
-    class ViewControllerFactoryStub: ViewControllerFactory {
-        
+    private class ViewControllerFactoryStub: ViewControllerFactory {
         private var stubbedQuestions = Dictionary<Question<String>, UIViewController>()
         private var stubbedResults = Dictionary<[Question<String>], UIViewController>()
 		
