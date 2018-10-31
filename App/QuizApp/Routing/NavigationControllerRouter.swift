69503bdfe79f6059946faa410e8e09d15d119d3f
Caio Zullo <caiozullo@gmail.com>
2018-10-31 10:23:10 +0000

    Implemented the didCompleteQuiz method in preparation to replace the deprecated Router implementation

--- App/QuizApp/Routing/NavigationControllerRouter.swift
+++ App/QuizApp/Routing/NavigationControllerRouter.swift
@@ -33,6 +33,10 @@ class NavigationControllerRouter: Router {
     func routeTo(question: Question<String>, answerCallback: @escaping ([String]) -> Void) {
         answer(for: question, completion: answerCallback)
     }
+	
+	func didCompleteQuiz(withAnswers answers: [(question: QuizEngine.Question<String>, answers: [String])]) {
+		show(factory.resultsViewController(for: answers))
+	}
     
     func routeTo(result: Result<Question<String>, [String]>) {
         show(factory.resultsViewController(for: result))
--- App/QuizAppTests/Routing/NavigationControllerRouterTest.swift
+++ App/QuizAppTests/Routing/NavigationControllerRouterTest.swift
@@ -98,16 +98,16 @@ class NavigationControllerRouterTest: XCTestCase {
 
     func test_routeToResult_showsResultController() {
         let viewController = UIViewController()
-        let result = Result.make(answers: [singleAnswerQuestion: ["A1"]], score: 10)
+        let userAnswers = [(singleAnswerQuestion, ["A1"])]
         
         let secondViewController = UIViewController()
-        let secondResult = Result.make(answers: [singleAnswerQuestion: ["A2"]], score: 20)
+        let secondUserAnswers = [(multipleAnswerQuestion, ["A2"])]
 
-        factory.stub(result: result, with: viewController)
-        factory.stub(result: secondResult, with: secondViewController)
+        factory.stub(resultForQuestions: [singleAnswerQuestion], with: viewController)
+        factory.stub(resultForQuestions: [multipleAnswerQuestion], with: secondViewController)
         
-        sut.routeTo(result: result)
-        sut.routeTo(result: secondResult)
+        sut.didCompleteQuiz(withAnswers: userAnswers)
+        sut.didCompleteQuiz(withAnswers: secondUserAnswers)
         
         XCTAssertEqual(navigationController.viewControllers.count, 2)
         XCTAssertEqual(navigationController.viewControllers.first, viewController)
@@ -125,15 +125,16 @@ class NavigationControllerRouterTest: XCTestCase {
     class ViewControllerFactoryStub: ViewControllerFactory {
         
         private var stubbedQuestions = Dictionary<Question<String>, UIViewController>()
-        private var stubbedResults = Dictionary<Result<Question<String>, [String]>, UIViewController>()
+        private var stubbedResults = Dictionary<[Question<String>], UIViewController>()
+		
         var answerCallback = Dictionary<Question<String>, ([String]) -> Void>()
         
         func stub(question: Question<String>, with viewController: UIViewController) {
             stubbedQuestions[question] = viewController
         }
         
-        func stub(result: Result<Question<String>, [String]>, with viewController: UIViewController) {
-            stubbedResults[result] = viewController
+        func stub(resultForQuestions questions: [Question<String>], with viewController: UIViewController) {
+            stubbedResults[questions] = viewController
         }
 
         func questionViewController(for question: Question<String>, answerCallback: @escaping ([String]) -> Void) -> UIViewController {
@@ -142,11 +143,11 @@ class NavigationControllerRouterTest: XCTestCase {
         }
 		
 		func resultsViewController(for userAnswers: Answers) -> UIViewController {
-			return UIViewController()
+			return stubbedResults[userAnswers.map { $0.question }] ?? UIViewController()
 		}
 		
         func resultsViewController(for result: Result<Question<String>, [String]>) -> UIViewController {
-            return stubbedResults[result] ?? UIViewController()
+            return UIViewController()
         }
     }
 
