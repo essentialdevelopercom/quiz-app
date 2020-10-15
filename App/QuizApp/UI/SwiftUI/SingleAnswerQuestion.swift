9af62c4dcb6726fb18ef73c12fe9d73dcc5abafe
Caio Zullo <caiozullo@gmail.com>
2020-10-15 16:26:53 +0100

    Extract QuestionHeader subview

--- App/QuizApp/UI/SwiftUI/SingleAnswerQuestion.swift
+++ App/QuizApp/UI/SwiftUI/SingleAnswerQuestion.swift
@@ -12,17 +12,7 @@ struct SingleAnswerQuestion: View {
 
     var body: some View {
         VStack(alignment: .leading, spacing: 0.0) {
-            VStack(alignment: .leading, spacing: 16.0) {
-                Text(title)
-                    .font(.headline)
-                    .fontWeight(.medium)
-                    .foregroundColor(Color.blue)
-                    .padding(.top)
-                
-                Text(question)
-                    .font(.largeTitle)
-                    .fontWeight(.medium)
-            }.padding()
+            QuestionHeader(title: title, question: question)
             
             ForEach(options, id: \.self) { option in
                 Button(action: {}, label: {
