3b57b17a7c736c1bd380e388b54dafe0a78457f5
Caio Zullo <caiozullo@gmail.com>
2020-10-15 16:34:08 +0100

    Add interactable SingleAnswerQuestionTestView preview

--- App/QuizApp/UI/SwiftUI/SingleAnswerQuestion.swift
+++ App/QuizApp/UI/SwiftUI/SingleAnswerQuestion.swift
@@ -15,7 +15,9 @@ struct SingleAnswerQuestion: View {
             QuestionHeader(title: title, question: question)
             
             ForEach(options, id: \.self) { option in
-                SingleTextSelectionCell(text: option, selection: {})
+                SingleTextSelectionCell(text: option, selection: {
+                    selection(option)
+                })
             }
             
             Spacer()
@@ -26,29 +28,32 @@ struct SingleAnswerQuestion: View {
 struct SingleAnswerQuestion_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            SingleAnswerQuestion(
-                title: "1 of 2",
-                question: "What's Mike's nationality?",
-                options: [
-                    "Portuguese",
-                    "American",
-                    "Greek",
-                    "Canadian"
-                ], selection: { _ in }
-            )
+            SingleAnswerQuestionTestView()
             
-            SingleAnswerQuestion(
-                title: "1 of 2",
-                question: "What's Mike's nationality?",
-                options: [
-                    "Portuguese",
-                    "American",
-                    "Greek",
-                    "Canadian"
-                ], selection: { _ in }
-            )
-            .preferredColorScheme(.dark)
-            .environment(\.sizeCategory, .extraExtraExtraLarge)
+            SingleAnswerQuestionTestView()
+                .preferredColorScheme(.dark)
+                .environment(\.sizeCategory, .extraExtraExtraLarge)
+        }
+    }
+    
+    struct SingleAnswerQuestionTestView: View {
+        @State var selection: String = "none"
+        
+        var body: some View {
+            VStack {
+                SingleAnswerQuestion(
+                    title: "1 of 2",
+                    question: "What's Mike's nationality?",
+                    options: [
+                        "Portuguese",
+                        "American",
+                        "Greek",
+                        "Canadian"
+                    ], selection: { selection = $0 }
+                )
+                
+                Text("Last selection: " + selection)
+            }
         }
     }
 }
