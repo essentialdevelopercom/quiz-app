3b0cbf3570230861ffc57770a3020b7979099b86
Caio Zullo <caiozullo@gmail.com>
2020-10-29 15:29:53 +0000

    Move MultipleSelectionStore to prod target

---
+++ App/QuizApp/UI/SwiftUI/MultipleSelectionStore.swift
@@ -0,0 +1,35 @@
+//	
+// Copyright © 2020 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+struct MultipleSelectionStore {
+    var options: [MultipleSelectionOption]
+    
+    var canSubmit: Bool {
+        !options.filter(\.isSelected).isEmpty
+    }
+    
+    private let handler: ([String]) -> Void
+    
+    init(options: [String], handler: @escaping ([String]) -> Void = { _ in }) {
+        self.options = options.map { MultipleSelectionOption(text: $0) }
+        self.handler = handler
+    }
+    
+    func submit() {
+        guard canSubmit else { return }
+        
+        handler(options.filter(\.isSelected).map(\.text))
+    }
+}
+
+struct MultipleSelectionOption {
+    let text: String
+    var isSelected = false
+    
+    mutating func select() {
+        isSelected.toggle()
+    }
+}
--- App/QuizAppTests/UI/SwiftUI/MultipleSelectionStoreTests.swift
+++ App/QuizAppTests/UI/SwiftUI/MultipleSelectionStoreTests.swift
@@ -3,36 +3,7 @@
 //
 
 import XCTest
-
-struct MultipleSelectionStore {
-    var options: [MultipleSelectionOption]
-    
-    var canSubmit: Bool {
-        !options.filter(\.isSelected).isEmpty
-    }
-    
-    private let handler: ([String]) -> Void
-    
-    init(options: [String], handler: @escaping ([String]) -> Void = { _ in }) {
-        self.options = options.map { MultipleSelectionOption(text: $0) }
-        self.handler = handler
-    }
-    
-    func submit() {
-        guard canSubmit else { return }
-        
-        handler(options.filter(\.isSelected).map(\.text))
-    }
-}
-
-struct MultipleSelectionOption {
-    let text: String
-    var isSelected = false
-    
-    mutating func select() {
-        isSelected.toggle()
-    }
-}
+@testable import QuizApp
 
 class MultipleSelectionStoreTests: XCTestCase {
 
