e4afa992334f53072f323b70025875376ee9b6c2
Caio Zullo <caiozullo@gmail.com>
2020-11-10 15:17:38 +0000

    Extract RoundedButton subview

--- App/QuizApp/UI/SwiftUI/Question/MultipleAnswerQuestion.swift
+++ App/QuizApp/UI/SwiftUI/Question/MultipleAnswerQuestion.swift
@@ -19,20 +19,12 @@ struct MultipleAnswerQuestion: View {
             
             Spacer()
             
-            Button(action: store.submit, label: {
-                HStack {
-                    Spacer()
-                    Text("Submit")
-                        .padding()
-                        .foregroundColor(Color.white)
-                    Spacer()
-                }
-                .background(Color.blue)
-                .cornerRadius(25)
-            })
-            .buttonStyle(PlainButtonStyle())
-            .padding()
-            .disabled(!store.canSubmit)
+			RoundedButton(
+				title: "Submit",
+				isEnabled: store.canSubmit,
+				action: store.submit
+			)
+			.padding()
         }
     }
 }
