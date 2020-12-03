b0035caf020e3ee942830523a6661625917c6c91
Caio Zullo <caiozullo@gmail.com>
2020-12-03 15:33:54 +0000

    Prevent creating builder without a question

--- App/QuizAppTests/Scoring/BasicQuizBuilderTest.swift
+++ App/QuizAppTests/Scoring/BasicQuizBuilderTest.swift
@@ -3,23 +3,30 @@
 //
 
 import XCTest
+import QuizEngine
 
 struct BasicQuiz {
-	
+	let questions: [Question<String>]
 }
 
 struct BasicQuizBuilder {
-	func build() -> BasicQuiz? {
-		nil
+	private let questions: [Question<String>]
+	
+	init(singleAnswerQuestion: String) {
+		questions = [.singleAnswer(singleAnswerQuestion)]
+	}
+	
+	func build() -> BasicQuiz {
+		BasicQuiz(questions: questions)
 	}
 }
 
 class BasicQuizBuilderTest: XCTestCase {
 
-    func test_empty() {
-        let sut = BasicQuizBuilder()
+    func test_initWithSingleAnswerQuestion() {
+		let sut = BasicQuizBuilder(singleAnswerQuestion: "q1")
 		
-		XCTAssertNil(sut.build())
+		XCTAssertEqual(sut.build().questions, [.singleAnswer("q1")])
     }
 
 }
